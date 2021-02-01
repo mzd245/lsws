4ae71cbd8d8685becae82c3e5b7a905ee25ba8c1
Dark-Rail <78325848+Dark-Rail@users.noreply.github.com>
2021-02-01 04:01:21 +0330

    Add files via upload

---
+++ options.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+directory="$HOME/Wallpapers/"
+#you should change value of directory variable.
+HOMEUSER="$HOME"
+app="hsetroot"
+app_flag="-cover"
+#you should change your tool with hsetroot. and you should use flag/operand of your tool for setting wallpaper.
+IGNORE=""
+#for example: IGNORE="one.jpg two.jpg". those wallpapers does not set.(splited with space)
+WallpaperNameAddr="$HOME/.local/share/lsws/logs/current_wallpaper.txt"
+WallpaperLogs="$HOME/.local/share/lsws/logs/wallpaper_logs.txt"
+WallpapersRestoreLogs="$HOME/.local/share/lsws/logs/wallpaper_res_logs.txt"
+#it is better to do not change values of these three lines.
